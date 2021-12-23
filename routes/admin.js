21de0345bdb53e26e0a3203642ce9bc329188ef9
Renan Ramos Linhares <renanlinhares@sie.sc.gov.br>
2021-12-23 19:38:13 -0300

    developing pages section

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 const { path } = require('../app');
 var router = express.Router();
@@ -10,24 +11,35 @@ const partials = {
   search: 'components/search',
 };
 
-router.get(['/', '/login'], function (req, res, next) {
-    res.redirect('/login');
+router.get('/login', function (req, res, next) {
+  res.redirect('/login');
 });
 
-router.get(['/', '/dashboard'], function (req, res, next) {
+router.get('/', function (req, res, next) {
+  res.redirect('/admin/dashboard');
+});
+
+router.get('/dashboard', function (req, res, next) {
   const title = 'Dashboard!';
   const path = 'admin/dashboard';
   res.render(path, { title, partials });
+});
+
+router.get('/pages/:param', function (req, res, next) {
+  const param = req.params.param;
+  var page = {
+    all: { title: 'Pages' },
+    create: { title: 'Add New Page' },
+    attributes: { title: 'Attributes' }
+  }[param];
+  if(page.title) res.render(`admin/pages/${param}`, { title: page.title, partials });
+  else next(createError(404));
 });
 
 router.get('/pages', function (req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ teste: 'OK', type: "tudo certo!" }));
   // res.send('Pages'+'<hr />'+req+'<hr />'+res);
-});
-
-router.get('/pages/create', function (req, res, next) {
-  res.send('Pages Create');
 });
 
 module.exports = router;
