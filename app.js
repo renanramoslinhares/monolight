9b035417a0adca1cecbf488486ddde7963380fbf
Renan Ramos Linhares <renan.linhares@softplan.com.br>
2022-01-02 20:08:51 -0300

    Create router.js

--- app.js
+++ app.js
@@ -3,36 +3,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
-// under development
-// use less css
-// var lessMiddleware = require('less-middleware');
-// var logger = require('morgan');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-// under development
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// under development
-// app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // under development
-app.use('/sample', express.static(__dirname + '/node_modules/admin-lte/pages'));
-
-app.use('/admin-lte', express.static(__dirname + '/node_modules/admin-lte'));
-app.use('/dist', express.static(__dirname + '/node_modules/admin-lte/dist'));
-app.use('/images', express.static(__dirname + '/public/images'));
-app.use('/admin', require('./routes/admin'));
-app.use('/login', require('./routes/login'));
-app.use('/api', require('./routes/api'));
-app.use('/fileService', require('./routes/fileService'));
-// app.use('/img', express.static(__dirname + '/node_modules/bootstrap/dist/'));
-// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/', require('./router'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ router.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const router = express.Router();
+var path = require('path');
+
+// under development
+// use less css
+// var lessMiddleware = require('less-middleware');
+// var logger = require('morgan');
+
+// under development
+// app.use(logger('dev'));
+
+
+// app.use('/img', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
+// under development
+// app.use(lessMiddleware(path.join(__dirname, 'public')));
+router.use(express.static(path.join(__dirname, 'public')));
+
+router.use('/sample', express.static(__dirname + '/node_modules/admin-lte/pages'));
+router.use('/admin-lte', express.static(__dirname + '/node_modules/admin-lte'));
+router.use('/dist', express.static(__dirname + '/node_modules/admin-lte/dist'));
+
+router.use('/admin', require('./router/admin'));
+router.use('/login', require('./router/login'));
+router.use('/api', require('./router/api'));
+router.use('/fileService', require('./router/fileService'));
+
+module.exports = router;
